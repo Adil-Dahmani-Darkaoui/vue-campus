e81258f1bbc8b0d79a6e973eddc569f362a682e9
adarkaoui <adarkaoui@oph74.fr>
2021-02-24 12:07:44 +0100

    UPD - Local storage

--- src/store/store.ts
+++ src/store/store.ts
@@ -8,19 +8,24 @@ export interface State {
 }
 
 export const store = createStore<State>({
-    
+
     actions: {
         getCitiesAsync ({ commit, state }) {
-            if (state.cities.length == 0) {
+            if (localStorage.getItem('cities') == null) {
+                console.log('CALL API')
                 axios
-                .get('https://api.openweathermap.org/data/2.5/find?lat=' + process.env.VUE_APP_DEFAULT_LATITUDE + '&lon=' + process.env.VUE_APP_DEFAULT_LONGITUDE + '&cnt=20&cluster=yes&lang=fr&units=metric&APPID=' + process.env.VUE_APP_OW_APP_ID)
-                .then((citiesData) => {
-                    let cities = [];
-                    for (const { name, coord: {lat, lon}, weather: [{description: weather, icon: icon}], main: {temp: temperature}, dt: updatedAt } of citiesData.data.list) { 
-                        cities.push({ name, lat, lon, weather, icon, temperature, updatedAt: new Date(updatedAt * 1000) });
-                    }
-                    commit('SaveCities', cities)
-                })
+                    .get('https://api.openweathermap.org/data/2.5/find?lat=' + process.env.VUE_APP_DEFAULT_LATITUDE + '&lon=' + process.env.VUE_APP_DEFAULT_LONGITUDE + '&cnt=20&cluster=yes&lang=fr&units=metric&APPID=' + process.env.VUE_APP_OW_APP_ID)
+                    .then((citiesData) => {
+                        let cities = [];
+                        for (const { name, coord: {lat, lon}, weather: [{description: weather, icon: icon}], main: {temp: temperature}, dt: updatedAt } of citiesData.data.list) {
+                            cities.push({ name, lat, lon, weather, icon, temperature, updatedAt: new Date(updatedAt) });
+                        }
+                        commit('SaveCities', cities);
+                        localStorage.setItem('cities', JSON.stringify(cities)); //backup
+                    })
+            } else {
+                console.log('PAS CALL API');
+                commit('SaveCities', JSON.parse(localStorage.cities));
             }
         }
     },
